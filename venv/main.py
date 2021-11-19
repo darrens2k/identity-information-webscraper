dcee9642c4132698f95fc00f5e937b59ae9825e9
Darren Singh <darrennsingh@gmail.com>
2021-11-19 15:45:29 -0500

    Accessing Instagram
    Created function to access instaggram. Advising to use fake instagram account to avoid real account getting banned. Next steps are to have the function sign into the fake instagram account.

--- venv/main.py
+++ venv/main.py
@@ -1,14 +1,25 @@
 # import necessary modules
 import selenium
 from selenium import webdriver
+import time
 
 # define sample information about target person
 name = "Darren Singh"
-location = "Markham"
+location = "Toronto"
 Occupation = "Student"
 School = "York University"
 
-# begin opening webpages
+# locate chrome driver
 browser = webdriver.Chrome('C:\\Users\\d\\Downloads\\chromedriver_win32\\chromedriver.exe')
-url = 'https://www.google.ca'
-browser.get(url)
+
+# create a function to access and login to instagram (use a fake account for this so you're real one doesn't get banned)
+def instagram(username, password):
+    url = "https://www.instagram.com"
+    browser.get(url)
+    # allow time for page to load
+    time.sleep(5)
+
+    # locate the password and username text fields, then feed in the password and username for the account you are using
+
+
+instagram(1, 1)
